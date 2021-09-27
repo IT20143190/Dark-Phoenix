9782e6200464120324aed2a9481497dbdb98faa0
IT20142582 <it20142582@my.sliit.lk>
2021-09-27 10:56:40 +0530

    new

--- app/src/main/java/com/example/petcare/AddActivity.java
+++ app/src/main/java/com/example/petcare/AddActivity.java
@@ -11,6 +11,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 
+import com.basgeekball.awesomevalidation.AwesomeValidation;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.FirebaseDatabase;
@@ -18,17 +19,21 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.basgeekball.awesomevalidation.ValidationStyle.BASIC;
+
 public class AddActivity extends AppCompatActivity {
-
-
 
     EditText title,age,breed,gender,phone,price,purl;
     Button btnAdd,btnBack;
+
+    AwesomeValidation awesomeValidation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        awesomeValidation = new AwesomeValidation(BASIC);
 
         title = (EditText)findViewById(R.id.txtTitle);
         age = (EditText)findViewById(R.id.txtAge);
@@ -38,15 +43,11 @@ public class AddActivity extends AppCompatActivity {
         price = (EditText)findViewById(R.id.txtPrice);
         purl = (EditText)findViewById(R.id.txtImageUrl);
 
-
-
-
-
-
-
-
         btnAdd = (Button)findViewById(R.id.btnAdd);
         btnBack = (Button)findViewById(R.id.btnBack);
+
+        awesomeValidation.addValidation(AddActivity.this, R.id.txtPhone, "^[0-9]{10}$", R.string.err_tel);
+        awesomeValidation.addValidation(AddActivity.this, R.id.txtBreed, "[a-zA-Z\\s]+", R.string.err_bre);
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,32 +67,36 @@ public class AddActivity extends AppCompatActivity {
         });
     }
 
+
     private void insertData()
     {
-        Map<String,Object> map = new HashMap<>();
-        map.put("title",title.getText().toString());
-        map.put("age",age.getText().toString());
-        map.put("breed",breed.getText().toString());
-        map.put("gender",gender.getText().toString());
-        map.put("phone",phone.getText().toString());
-        map.put("price",price.getText().toString());
-        map.put("purl",purl.getText().toString());
+
+        if (awesomeValidation.validate()) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("title", title.getText().toString());
+            map.put("age", age.getText().toString());
+            map.put("breed", breed.getText().toString());
+            map.put("gender", gender.getText().toString());
+            map.put("phone", phone.getText().toString());
+            map.put("price", price.getText().toString());
+            map.put("purl", purl.getText().toString());
 
 
-        FirebaseDatabase.getInstance().getReference().child("pet").push()
-                .setValue(map)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void unused) {
-                        Toast.makeText(AddActivity.this, "Data Inserted Successfully", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(AddActivity.this, "Error While Inserting Data", Toast.LENGTH_SHORT).show();
-                    }
-                });
+            FirebaseDatabase.getInstance().getReference().child("pet").push()
+                    .setValue(map)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Toast.makeText(AddActivity.this, "Data Inserted Successfully", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(AddActivity.this, "Error While Inserting Data", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
     }
 
     private void clearAll()
